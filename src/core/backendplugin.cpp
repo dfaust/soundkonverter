161f351050530cd526e66f6f7d9f5a03a44b11ce
Daniel Faust <hessijames@gmail.com>
2012-11-08 18:19:17 +0100

    Add alac as extions to m4a/alac

--- src/core/backendplugin.cpp
+++ src/core/backendplugin.cpp
@@ -68,6 +68,7 @@ BackendPlugin::FormatInfo BackendPlugin::formatInfo( const QString& codecName )
         info.mimeTypes.append( "audio/x-m4a" );
         info.extensions.append( "m4a" );
         info.extensions.append( "f4a" );
+        info.extensions.append( "alac" );
         info.extensions.append( "mp4" );
     }
     else if( codecName == "mp4" )

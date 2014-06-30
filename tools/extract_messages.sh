ca0d5cf6b09cb6d4a07c72b23a776fd88535ba90
Björn Esser <bjoern.esser@gmail.com>
2014-06-30 10:54:16 +0200

    add GPLv2-license headers

--- tools/extract_messages.sh
+++ tools/extract_messages.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
+#
+# Copyright (c) 2010 - 2014 Daniel Faust <hessijames@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 # http://techbase.kde.org/Development/Tutorials/Localization/i18n_Build_Systems
+
 BASEDIR="../src/" # root of translatable sources
 PROJECT="soundkonverter" # project name
 BUGADDR="https://bugs.launchpad.net/ubuntu/%2Bsource/soundkonverter/" # MSGID-Bugs

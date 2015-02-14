bb194e8f8c19ebc1ced45a5e1a65252b37b9d2c4
valeth <patrick.auernig@gmail.com>
2015-02-14 17:35:21 +0100

    port the plugin loader to KF5

--- src/pluginloader.cpp
+++ src/pluginloader.cpp
@@ -128,7 +128,11 @@ void PluginLoader::load()
             QVariantList allArgs;
             allArgs << offers.at(i)->storageId() << "";
             QString error;
+#ifdef SOUNDKONVERTER_KF5_BUILD
+            CodecPlugin *plugin = offers.at(i).data()->createInstance<CodecPlugin>(0, allArgs, &error );
+#else
             CodecPlugin *plugin = KService::createInstance<CodecPlugin>( offers.at(i), 0, allArgs, &error );
+#endif
             if( plugin )
             {
                 logger->log( 1000, "\tloading plugin: " + plugin->name() );
@@ -188,7 +192,12 @@ void PluginLoader::load()
             QVariantList allArgs;
             allArgs << offers.at(i)->storageId() << "";
             QString error;
+
+#ifdef SOUNDKONVERTER_KF5_BUILD
+            FilterPlugin *plugin = offers.at(i).data()->createInstance<FilterPlugin>(0, allArgs, &error );
+#else
             FilterPlugin *plugin = KService::createInstance<FilterPlugin>( offers.at(i), 0, allArgs, &error );
+#endif
             if( plugin )
             {
                 logger->log( 1000, "\tloading plugin: " + plugin->name() );
@@ -249,7 +258,12 @@ void PluginLoader::load()
             QVariantList allArgs;
             allArgs << offers.at(i)->storageId() << "";
             QString error;
+
+#ifdef SOUNDKONVERTER_KF5_BUILD
+            ReplayGainPlugin *plugin = offers.at(i).data()->createInstance<ReplayGainPlugin>(0, allArgs, &error );
+#else
             ReplayGainPlugin *plugin = KService::createInstance<ReplayGainPlugin>( offers.at(i), 0, allArgs, &error );
+#endif
             if( plugin )
             {
                 logger->log( 1000, "\tloading plugin: " + plugin->name() );
@@ -285,7 +299,11 @@ void PluginLoader::load()
             QVariantList allArgs;
             allArgs << offers.at(i)->storageId() << "";
             QString error;
+#ifdef SOUNDKONVERTER_KF5_BUILD
+            RipperPlugin *plugin = offers.at(i).data()->createInstance<RipperPlugin>(0, allArgs, &error );
+#else
             RipperPlugin *plugin = KService::createInstance<RipperPlugin>( offers.at(i), 0, allArgs, &error );
+#endif
             if( plugin )
             {
                 logger->log( 1000, "\tloading plugin: " + plugin->name() );

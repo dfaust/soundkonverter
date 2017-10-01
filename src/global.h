aa20a5ad025cd30adaec16989ca4f8868da69f72
Daniel Faust <hessijames@gmail.com>
2017-10-01 10:39:49 +0200

    Update changelog and version number

--- src/global.h
+++ src/global.h
@@ -15,7 +15,7 @@
 #include <QString>
 
 #define SOUNDKONVERTER_VERSION_NUMBER 1006
-#define SOUNDKONVERTER_VERSION_STRING "3.0.0"
+#define SOUNDKONVERTER_VERSION_STRING "3.0.1"
 
 /**
 	@author Daniel Faust <hessijames@gmail.com>
